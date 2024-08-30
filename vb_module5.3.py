f2cab4e7cb3a553a84d998bd26dd955725c1c911
BezborodovVV37 <blood371993@gmail.com>
2024-08-30 13:36:11 +0300

    Add files via upload

--- vb_module5.3.py
+++ vb_module5.3.py
@@ -13,37 +13,42 @@ class House:
     def __str__(self):
         return f'Название:{self.name}, кол-во этажей: {self.number_of_floors}'
 
-    def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+    def __eq__(self, other):  # 1
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
 
-    def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+    def __lt__(self, other):  # 2
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
         if isinstance(value, int):
-            self.number_of_floors += value
-            return self
+            self.number_of_floors = self.number_of_floors + value
+        return self
 
     def __radd__(self, value):
-        self.number_of_floors += value
-        return self
+        return self.__add__(value)
 
     def __iadd__(self, value):
         if isinstance(value, int):
             self.number_of_floors += value
-            return self
+        return self
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -52,13 +57,13 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2)
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10
+h1 += 10  # __iadd__
 print(h1)
 
 h2 = 10 + h2  # __radd__
